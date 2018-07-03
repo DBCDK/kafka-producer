90024501f94e03fb22318e849d840d00a97a326b
Michael Willer <willer@lundogbendsen.dk>
2018-07-03 16:23:40 +0200

    Adding missing Exception.

---
+++ src/main/java/dk/dbc/kafka/exception/KafkaException.java
@@ -0,0 +1,11 @@
+package dk.dbc.kafka.exception;
+
+public class KafkaException extends RuntimeException {
+    public KafkaException(String message) {
+        super(message);
+    }
+
+    public KafkaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
